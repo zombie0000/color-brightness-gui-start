2d3cd05f5bc3c81b2ad5e567af504243b98414b4
Jonathan Luo <97814319+zombie0000@users.noreply.github.com>
2022-10-27 12:19:25 -0600

    scvxzcv

---
+++ main.js
@@ -0,0 +1,14 @@
+// CALCULATE BRIGHTNESS IPO CALCULATOR BY JONATHAN
+
+// Input
+let R = +prompt("Enter an R color value (Red):");
+let G = +prompt("Enter a G color value (Green):");
+let B = +prompt("Enter a B color value (Blue):");
+
+console.log(R);
+console.log(G);
+console.log(B);
+
+// Process
+
+// Output
