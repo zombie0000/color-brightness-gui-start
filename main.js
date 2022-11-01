87353ff0815db0bc22abffc3e9dce2c2a903d762
Jonathan Luo <97814319+zombie0000@users.noreply.github.com>
2022-11-01 14:32:35 -0600

    sadf

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 // CALCULATE BRIGHTNESS IPO CALCULATOR BY JONATHAN
 
-// Input
-let R = +prompt("Enter an R color value (Red):");
-let G = +prompt("Enter a G color value (Green):");
-let B = +prompt("Enter a B color value (Blue):");
+//Event Listener on the Button
+document.getElementById("btn").addEventListener("click", btnClicked);
 
-console.log(R);
-console.log(G);
-console.log(B);
+function btnClicked() {
+  // Input
+  let R = +document.getElementById("r-in").value;
+  let G = +document.getElementById("g-in").value;
+  let B = +document.getElementById("b-in").value;
 
-// Process
+  // Process
+  let brightness = Math.sqrt(0.299 * R ** 2 + 0.587 * G ** 2 + 0.114 * B ** 2);
 
-// Output
+  // Output
+  document.getElementById("output").innerHTML = brightness.toFixed(0);
+  document.body.style.backgroundColor = `rgb(${R}, %{G}, %{B})`;
+}
